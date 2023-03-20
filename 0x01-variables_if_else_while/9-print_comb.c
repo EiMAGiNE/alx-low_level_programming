26f69f1648bf1e62e5db7a84455e4aeca26b9d9e
EiMAGiNE <eimaginedlawise@yahoo.com>
2023-03-20 14:37:46 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,6 +14,7 @@ int main(void)
 	int i;
 
 	for (i = '0'; i <= '9'; i++)
+	{
 		putchar (i);
 
 		if (i != 9)
@@ -21,6 +22,6 @@ int main(void)
 			putchar(',');
 			putchar(' ');
 		}
-
+	}
 	return (0);
 }
