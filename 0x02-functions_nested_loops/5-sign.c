a9dbc8468b5e4f85ffd2e498217313423a4cf7fb
EiMAGiNE <eimaginedlawise@yahoo.com>
2023-03-22 16:22:12 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,24 +3,32 @@
 /**
  * print_sign - Entry point
  *
+ * @n: parameter is printed
+ *
  * Return: 1 is greater than zero. 0 is zero. -1 is less than zero
- */
+ /
 
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar(43);
+		_putchar(',');
+		_putchar(' ');
 		return (1);
 	}
 	else if (n < 0)
 	{
 		_putchar(45);
+		_putchar(',');
+		_putchar(' ');
 		return (-1);
 	}
 	else
 	{
 		_putchar(48);
+		_putchar(',');
+		_putchar(' ');
 		return (0);
 	}
 	_putchar('\n');
