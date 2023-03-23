81217ffb109cb77cee9f0e953cbddb67983d6c32
EiMAGiNE <eimaginedlawise@yahoo.com>
2023-03-22 17:11:52 -0700

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,9 +10,9 @@ void jack_bauer(void)
 {
 	int a, b, c, d;
 
-	for (a = 0 ; a <= 3 ; a++)
+	for (a = 0 ; a <= 2 ; a++)
 	{
-		for (b = 0 ; b <= 9 ; b++)
+		for (b = 0 ; b <= 4 ; b++)
 		{
 			for (c = 0 ; c <= 5 ; c++)
 			{
