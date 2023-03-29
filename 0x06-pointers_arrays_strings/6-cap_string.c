c6f420784e15967a9dbe37dd62728e5f478db542
EiMAGiNE <eimaginedlawise@yahoo.com>
2023-03-29 16:42:24 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,36 @@
 #include "main.h"
 
 /**
- * cap_string -
+ * cap_string - capitalize string
+ *
+ * @str: parameter to be capitalized
+ *
+ * Return: string
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+
+	for (str[i] >= 'a' && str[i] <= 'z')
+		str[i] = str[i] - 32;
+	{
+		for (i = 0 ; str[i] != '\0' ; i++)
+		{
+			str[i] == ' ' ||
+			str[i] == '\t' ||
+			str[i] == '\n' ||
+			str[i] == ',' ||
+			str[i] == ';' ||
+			srt[i] == '.' ||
+			str[i] == '!' ||
+			str[i] == '?' ||
+			str[i] == '"' ||
+			str[i] == '(' ||
+			str[i] == ')' ||
+			str[i] == '{' ||
+			str[i] == '}' ||
+		}
+	}
+	return (str);
+}
