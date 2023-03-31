99880bdc5134f5a77c5c91ca79b1cb9b7089ebb7
EiMAGiNE <eimaginedlawise@yahoo.com>
2023-03-30 17:45:59 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,26 +10,29 @@
 
 char *cap_string(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (str[i] >= 'a' && str[i] <= 'z')
-		str[i] = str[i] - 32;
+	for (!(str[i] >= 'a' && str[i] <= 'z'))
 	{
 		for (i = 0 ; str[i] != '\0' ; i++)
 		{
-			str[i] == ' ' ||
-			str[i] == '\t' ||
-			str[i] == '\n' ||
-			str[i] == ',' ||
-			str[i] == ';' ||
-			srt[i] == '.' ||
-			str[i] == '!' ||
-			str[i] == '?' ||
-			str[i] == '"' ||
-			str[i] == '(' ||
-			str[i] == ')' ||
-			str[i] == '{' ||
-			str[i] == '}' ||
+			if 	(str[i - 1] == ' ' ||
+				str[i - 1] == '\t' ||
+				str[i - 1] == '\n' ||
+				str[i - 1] == ',' ||
+				str[i - 1] == ';' ||
+				srt[i - 1] == '.' ||
+				str[i - 1] == '!' ||
+				str[i - 1] == '?' ||
+				str[i - 1] == '"' ||
+				str[i - 1] == '(' ||
+				str[i - 1] == ')' ||
+				str[i - 1] == '{' ||
+				str[i - 1] == '}' ||
+				i == 0)
+
+				str[i] = str[i] - 32;
+				i++;
 		}
 	}
 	return (str);
