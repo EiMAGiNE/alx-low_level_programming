37369b9021f39aee8adb6f8a3c3464c6018ad8b3
EiMAGiNE <eimaginedlawise@yahoo.com>
2023-04-18 13:35:54 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - decribe a new type of dog
+ * @name: parameter for name
+ * @age: parameter for age
+ * @owner: parameter for owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
