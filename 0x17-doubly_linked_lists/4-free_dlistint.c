d0fab397dfb79ca96f3cf9adf33ec7c30209b6d9
EiMAGiNE <eimaginedlawise@yahoo.com>
2023-06-15 11:08:14 -0700

    Free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees list
+ * @head - parameter for head pointer
+ *
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head;
+
+	while (current != NULL)
+	{
+		dlistint_t *next = current->next;
+		free(current);
+		current = next;
+	}
+}
