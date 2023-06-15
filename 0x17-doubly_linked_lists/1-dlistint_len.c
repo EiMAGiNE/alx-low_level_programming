836ac6fe97c26d9b1393a05d8362669900505e37
EiMAGiNE <eimaginedlawise@yahoo.com>
2023-06-15 08:26:42 -0700

    List length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns number of element in linked list
+ * @h: parameter for head
+ *
+ * Return: number of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
