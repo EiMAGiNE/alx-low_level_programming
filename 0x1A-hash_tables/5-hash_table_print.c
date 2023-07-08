7342c690f1d6a40ec1ffe04b26cba00de6958c44
EiMAGiNE <eimaginedlawise@yahoo.com>
2023-07-07 17:53:43 -0700

    Hash Tables

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -18,7 +18,7 @@ void hash_table_print(const hash_table_t *ht)
 		return;
 
 	printf("{");
-	
+
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
@@ -27,12 +27,12 @@ void hash_table_print(const hash_table_t *ht)
 				printf(", ");
 
 			node = ht->array[i];
-			
+
 			while (node != NULL)
 			{
 				printf("'%s': '%s'", node->key, node->value);
 				node = node->next;
-				
+
 				if (node != NULL)
 					printf(", ");
 			}
