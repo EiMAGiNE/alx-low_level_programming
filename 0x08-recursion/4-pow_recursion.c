72a91e7d2cfe42a86d4f54f0c0e5410210bdce3c
dlawise <eimaginedlawise@yahoo.com>
2024-01-10 15:12:12 -0800

    Update

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,12 +7,18 @@
  *
  * Return: the result of the power
  */
-int _pow_recursion(int x, int y) {
-    if (y < 0) {
-        return -1;
-    } else if (y == 0) {
-        return 1;
-    } else {
-        return x * _pow_recursion(x, y - 1);
-    }
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
 }
